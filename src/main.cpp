9bbaf0ef490a8e9c525c6bbf5f5f342ae5d96b1a
T3kla <manuillo94@gmail.com>
2022-10-09 16:26:44 +0200

    Clanging but not debugging

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,9 @@ int main()
 
     GLFWwindow *window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
 
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::cout << extensionCount << " extensions supported\n";
+    // uint32_t extensionCount = 0;
+    // vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    // std::cout << extensionCount << " extensions supported\n";
 
     while (!glfwWindowShouldClose(window))
     {
