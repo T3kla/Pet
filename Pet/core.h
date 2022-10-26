2c92562b22e784931e560a9b9721e8d0cd3cf473
T3kla <manuillo94@gmail.com>
2022-10-27 01:16:21 +0200

    More weird usings

--- Pet/core.h
+++ Pet/core.h
@@ -10,7 +10,15 @@
 #include <iostream>
 #include <stdint.h>
 #include <string.h>
+
+#include <array>
 #include <vector>
+
+#include <set>
+#include <unordered_set>
+
+#include <map>
+#include <unordered_map>
 
 using i16 = int16_t;               //
 using i32 = int32_t;               //
@@ -19,8 +27,23 @@ using u16 = uint16_t;              //
 using u32 = uint32_t;              //
 using u64 = uint64_t;              //
 using job = std::function<void()>; //
-template <typename... Ts>          //
-using list = std::vector<Ts...>;   //
+
+template <typename... Ts>                   //
+using array = std::array<Ts...>;            //
+template <typename... Ts>                   //
+using list = std::vector<Ts...>;            //
+template <typename... Ts>                   //
+using oset = std::set<Ts...>;               //
+template <typename... Ts>                   //
+using set = std::unordered_set<Ts...>;      //
+template <typename... Ts>                   //
+using odic = std::map<Ts...>;               //
+template <typename... Ts>                   //
+using dic = std::unordered_map<Ts...>;      //
+template <typename... Ts>                   //
+using omap = std::multimap<Ts...>;          //
+template <typename... Ts>                   //
+using map = std::unordered_multimap<Ts...>; //
 
 #ifdef _DEBUG
 const bool Debug = true;
